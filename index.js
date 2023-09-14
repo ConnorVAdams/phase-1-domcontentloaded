f9a380b296f3008cc6b83600939f92b3dd7baa20
Connor Adams <connorvadams@gmail.com>
2023-09-13 21:52:46 -0600

    Changes the p element after DOM loads.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code goes here
+//Adds event listener to log when DOM is loaded.
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("The DOM has loaded.");
+});
+
+//Proves that the Event Handler defined above doesn't execute until after the DOM is loaded.
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered."
+);
+
+//Changes the text in body > p.
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('#text').textContent = "This is really cool!"
+});
